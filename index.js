ac88c22726be82e38e7886a4e331d47649e6e159
Remmy Clay <remmy.lauren@gmail.com>
2017-10-30 22:49:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget(nested, target) {
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  const findLis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < findLis.length; i++) {
+    findLis[i].innerHTML = parseInt(findLis[i].innerHTML)+n;
+  }
+}
+
+function deepestChild() {
+  var grandNodeChild = document.getElementById('grand-node').querySelectorAll('div');
+  for (let i = 0; i < grandNodeChild.length; i++) {
+    if (grandNodeChild[i].querySelectorAll('div').length  === 0) {
+      return grandNodeChild[i];
+    }
+  }
+}
